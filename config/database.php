22c5da7eacb6a6140b3ee78e79d5d383586d87f4
Carlos Javier Pérez Mutuberría <cmutuberria@gmail.com>
2019-05-21 11:42:26 -0400

    database para deploy en heroku

--- config/database.php
+++ config/database.php
@@ -37,11 +37,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
+            'host' => env('DB_HOST', 'mysql-2606-0.cloudclusters.net'),
+            'port' => env('DB_PORT', '10003'),
             'database' => env('DB_DATABASE', 'comercial'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'username' => env('DB_USERNAME', 'comercial'),
+            'password' => env('DB_PASSWORD', 'comercial'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
